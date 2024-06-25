5fdf5b8fa4deabff7bc01e947f2d0944d78a11d9
enoquerogerio <enoquerogerio15mti25@gmail.com>
2024-06-26 00:35:56 +0100

    setup certificates for https

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express, { Express } from "express";
+import https from "https";
 import path from "path";
+import fs from "fs";
 import cookieParser from "cookie-parser";
 import { PORT } from "./secrets";
 import rootRouter from "./routes";
@@ -12,6 +14,17 @@ app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
 app.use("/api", rootRouter);
 
 initialData();
-app.listen(PORT, () => {
-  console.log("HTTP server running!");
+app.get("/", (req, res) => {
+  return res.send("Messi");
 });
+https
+  .createServer(
+    {
+      key: fs.readFileSync("certificates/key.pem"),
+      cert: fs.readFileSync("certificates/cert.pem"),
+    },
+    app
+  )
+  .listen(PORT, () => {
+    console.log("HTTPS server running!");
+  });
