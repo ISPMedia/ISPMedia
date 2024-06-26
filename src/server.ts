8a6dd31b4782b13445b0c779d76ad708fb3b6044
enoquerogerio <enoquerogerio15mti25@gmail.com>
2024-06-26 01:02:15 +0100

    setup security with helmet and cors

--- src/server.ts
+++ src/server.ts
@@ -2,21 +2,26 @@ import express, { Express } from "express";
 import https from "https";
 import path from "path";
 import fs from "fs";
+import helmet from "helmet";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import { PORT } from "./secrets";
 import rootRouter from "./routes";
 import { initialData } from "./data/ispmedia";
 const app: Express = express();
 
+app.use(cors());
+app.use(
+  helmet({
+    crossOriginResourcePolicy: false,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
 app.use("/api", rootRouter);
 
 initialData();
-app.get("/", (req, res) => {
-  return res.send("Messi");
-});
 https
   .createServer(
     {
